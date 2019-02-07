a9b557b28823abfd55a336f955cd81d9e4455a3b
karabas2903 <39926420+karabas2903@users.noreply.github.com>
2019-02-07 14:17:21 +0200

    Update

--- WCIIRemake/AStar.h
+++ WCIIRemake/AStar.h
@@ -31,23 +31,23 @@ class AStar : public Obj {
 		bool **closedList;
 		typedef pair<int, int> Pair;
 		typedef pair<double, pair<int, int>> pPair;
-		bool isValid(int row, int col); // проверяет, есть ли такая координата на карте(тоесть существует ли ячейка с такими коорднатами)
-		bool isUnBlocked(int row, int col); // проверяет, доступна ли ячейка
-		bool isDestination(int row, int col, cordScr dest); // проверяет, достигнута ли цель
-		double calculateHValue(int row, int col, cordScr dest);// считает прямое расстояние по пифагору
-		void tracePath(cordScr dest); // показывает путь
+		bool isValid(int row, int col); // Returns true if row number and column number is in range 
+		bool isUnBlocked(int row, int col); // check whether the given cell is blocked or not 
+		bool isDestination(int row, int col, cordScr dest); // check whether destination cell has been reached or not
+		double calculateHValue(int row, int col, cordScr dest); // Return using the distance formula of Pifagor :)
+		void tracePath(cordScr dest); // // trace the path from the start to destination 
 	protected:
 		char ** path;
 		int **grid;
 		int direction;
 	public:
-		void getPathMap(); // показать путь(дебаг)
-		void getMap(char * field, char type); // для преобразование в булевую карту
+		void getPathMap(); //show path(debug)
+		void getMap(char * field, char type); // for transformation in bool map
 		int rows;
 		int columns;
-		AStar(int h, int w); // конструктор класса
+		AStar(int h, int w);
 		~AStar();
-		void Dijkstra(cordScr start, cordScr dest); // поиск пути по дейкстре(пока не написан)
-		int getPath();// возвращает 8 направлений 1 - up, 2 - down, 3 - right, 4 - left, 5 - north east, 6 - north west, 7 - south east, 8 - south west
-		void aStarSearch(cordScr start, cordScr dest); // показывает путь с заданного координатами старта до финиша
+		void Dijkstra(cordScr start, cordScr dest); // path search of Dijkstra(not developed)
+		int getPath();// returns 1 - up, 2 - down, 3 - right, 4 - left, 5 - north east, 6 - north west, 7 - south east, 8 - south west
+		void aStarSearch(cordScr start, cordScr dest); // main function for astar search, from start to destination
 };
